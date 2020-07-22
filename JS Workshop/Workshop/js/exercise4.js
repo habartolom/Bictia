bb4d2e448ed3b0da4890c59f018a1feeaf168851
Harold Andrés Bartolo Moscoso <habartolom@unal.edu.co>
2020-07-22 00:55:01 -0500

    JS Workshop

--- JS Workshop/Workshop/js/exercise4.js
+++ JS Workshop/Workshop/js/exercise4.js
@@ -1,76 +1,69 @@
-class User{
-    constructor(name, mail, dir, age, occupation, tel){
-        this.name = name;
-        this.mail = mail;
-        this.dir = dir;
-        this.age = age;
-        this.occupation = occupation;
-        this.tel = tel;
+
+const name_input = document.getElementById("name_input");
+const mail_input = document.getElementById("mail_input");
+const dir_input = document.getElementById("dir_input");
+const age_input = document.getElementById("age_input");
+const occupation_input = document.getElementById("occupation_input");
+const tel_input = document.getElementById("tel_input");
+
+class User {
+    constructor() {
+        this.name = name_input.value;
+        this.mail = mail_input.value;
+        this.dir = dir_input.value;
+        this.age = age_input.value;
+        this.occupation = occupation_input.value;
+        this.tel = tel_input.value;
     }
 }
 
-const name_input =  document.getElementById("name_input");
-const mail_input =  document.getElementById("mail_input");
-const dir_input =  document.getElementById("dir_input");
-const age_input =  document.getElementById("age_input");
-const occupation_input =  document.getElementById("occupation_input");
-const tel_input =  document.getElementById("tel_input");
-
-function createMessage(alert, message){
+function createMessage(alert, message) {
     alert.innerText = message;
 }
 
-function isEmpty(element){
-    let isValid = true;
+function isEmpty(element) {
+    let isEmpty = false;
     let alert = element.nextElementSibling;
     createMessage(alert, "");
-    if (element.value == ""){
+    if (element.value == "") {
         createMessage(alert, "El campo se encuentra vacío.");
-        isValid = false;
+        isEmpty = true;
     }
-    return isValid;  
+    return isEmpty;
 }
 
-function isNumeric(element){
+function isValidNumeric(element) {
     let isValid = true;
     let alert = element.nextElementSibling;
     createMessage(alert, "");
-    if (Number.isNaN(Number(element.value))){
+    if (Number.isNaN(Number(element.value))) {
         createMessage(alert, "Ingrese un valor numérico.");
         isValid = false;
     }
-    return isValid;  
-}
-
-function nameIsValid(){
-    return isEmpty(name_input);
-} 
-
-function mailIsValid(){
-    return isEmpty(mail_input);
-}
-
-function dirIsValid() {
-    return isEmpty(dir_input);
-}
-
-function ageIsValid(){
-    return isEmpty(age_input) && isNumeric(age_input);
-}
-
-function occupationIsValid(){
-    return isEmpty(occupation_input);
-} 
-
-function telIsValid(){
-    return isEmpty(tel_input) && isNumeric(tel_input);
-}
-
-
-function setUser(){
-    if(nameIsValid() & mailIsValid()& dirIsValid() & ageIsValid() & occupationIsValid() & telIsValid()){
-        var user = new User(name_input.value, mail_input.value, dir_input.value, age_input.value, occupation_input.value, tel_input.value)
-        console.log(user);
+    else if (Number(element.value) <= 0) {
+        createMessage(alert, "Ingrese un valor mayor a 0.");
+        isValid = false;
     }
-    
+    return isValid;
+}
+
+function cleanFields() {
+    name_input.value = "";
+    mail_input.value = "";
+    dir_input.value = "";
+    age_input.value = "";
+    occupation_input.value = "";
+    tel_input.value = "";
+}
+
+function objectIsValid() {
+    return !isEmpty(name_input) & !isEmpty(mail_input) & !isEmpty(dir_input) & (!isEmpty(age_input) && isValidNumeric(age_input)) & !isEmpty(occupation_input) & (!isEmpty(tel_input) && isValidNumeric(tel_input));
+}
+
+function setUser() {
+    if (objectIsValid()) {
+        var user = new User();
+        console.log(user);
+        cleanFields();
+    }
 }
